520632d31be608c9ee53244dcc2f8c75fb33dcf1
Felipe Costa <felipe.costa@sap.com>
2022-12-07 10:55:55 -0300

    Day 07

--- solution/Day07.cs
+++ solution/Day07.cs
@@ -1,50 +1,34 @@
 interface IFileSystemNode
 {
   public string Name { get; init; }
-  public HashSet<IFileSystemNode> Children { get; init; }
-  public long CalculateSize();
-  public bool IsDirectory { get; }
-  public IFileSystemNode? Parent { get; set; }
+  public long Size {get;}
+  public Directory? Parent { get; set; }
 }
 
 class Directory : IFileSystemNode
 {
   public string Name { get; init; } = "";
   public HashSet<IFileSystemNode> Children { get; init; } = new();
-  public IFileSystemNode? Parent { get; set; }
-  private long _sizeCache = -1;
-
-  public long CalculateSize()
-  {
-    if (_sizeCache == -1)
-    {
-      _sizeCache = Children.Sum(child => child.CalculateSize());
-    }
-    return _sizeCache;
-  }
-  public bool IsDirectory => true;
+  public Directory? Parent { get; set; }
+  public long Size => Children.Sum(child => child.Size);
 }
 
-class File : IFileSystemNode
+record File(string Name, long Size) : IFileSystemNode
 {
-  public string Name { get; init; } = "";
-  public long Size { get; set; }
-  public HashSet<IFileSystemNode> Children { get; init; } = new();
-  public IFileSystemNode? Parent { get; set; }
+  public Directory? Parent { get; set; }
 
   public long CalculateSize() => Size;
-  public bool IsDirectory => false;
 }
 
 class FileSystem
 {
-  public IFileSystemNode Root { get; init; } = new Directory() { Name = "/" };
+  public Directory Root { get; init; } = new Directory() { Name = "/" };
   public HashSet<IFileSystemNode> Nodes = new();
-  private IFileSystemNode _currentNode { get; set; }
+  private Directory _currentDirectory { get; set; }
 
   public FileSystem()
   {
-    _currentNode = Root;
+    _currentDirectory = Root;
   }
 
   public void ProcessCommand(string command)
@@ -64,28 +48,28 @@ class FileSystem
   {
     var newNode = words[0] switch
     {
-      "dir" => (IFileSystemNode)new Directory() { Name = words[1], Parent = _currentNode },
-      _ => (IFileSystemNode)new File() { Size = long.Parse(words[0]), Name = words[1], Parent = _currentNode }
+      "dir" => (IFileSystemNode)new Directory() { Name = words[1], Parent = _currentDirectory },
+      _ => (IFileSystemNode)new File(words[1], long.Parse(words[0])) { Parent = _currentDirectory }
     };
 
-    var existingNode = _currentNode.Children.FirstOrDefault(node => node.Name == words[1]);
+    var existingNode = _currentDirectory.Children.FirstOrDefault(node => node.Name == words[1]);
 
     if (existingNode is not null)
     {
       newNode = existingNode;
     }
 
-    _currentNode.Children.Add(newNode);
+    _currentDirectory.Children.Add(newNode);
     Nodes.Add(newNode);
   }
 
   private void _processCurrentDir(string[] words)
   {
-    _currentNode = words[2] switch
+    _currentDirectory = words[2] switch
     {
-      "/" => _currentNode = Root,
-      ".." => _currentNode.Parent ?? Root,
-      _ => _currentNode.Children.FirstOrDefault(child => child.Name == words[2]) ?? _currentNode,
+      "/" => _currentDirectory = Root,
+      ".." => _currentDirectory.Parent ?? Root,
+      _ => (Directory) (_currentDirectory.Children.FirstOrDefault(child => child.Name == words[2] && child is Directory) ?? _currentDirectory),
     };
   }
 }
@@ -100,27 +84,27 @@ class Day07 : IDayCommand
     lines.ForEach(line => fileSystem.ProcessCommand(line));
 
     var sizes = fileSystem.Nodes
-      .Where(node => node.IsDirectory)
-      .Select(node => node.CalculateSize())
+      .Where(node => node is Directory)
+      .Select(node => node.Size)
       .ToList();
 
     var resultPart01 = fileSystem.Nodes
-      .Where(node => node.IsDirectory)
-      .Select(node => node.CalculateSize())
+      .Where(node => node is Directory)
+      .Select(node => node.Size)
       .Where(size => size <= 100000)
       .Sum();
 
-    var totalSpaceUsed = fileSystem.Root.CalculateSize();
+    var totalSpaceUsed = fileSystem.Root.Size;
     var spaceThatNeedsToBeFreed = totalSpaceUsed - (70000000 - 30000000);
     
     var smallestDirectoryToDelete = fileSystem.Nodes
-      .Where(node => node.CalculateSize() >= spaceThatNeedsToBeFreed)
-      .OrderBy(node => node.CalculateSize())
+      .Where(node => node.Size >= spaceThatNeedsToBeFreed)
+      .OrderBy(node => node.Size)
       .First();
     
     return $"""
             The total size of directories with most 100000 of size is {resultPart01}.
-            the smallest dir that needs to be delete has size of {smallestDirectoryToDelete.CalculateSize()}
+            the smallest dir that needs to be delete has size of {smallestDirectoryToDelete.Size}
            """;
 
   }
